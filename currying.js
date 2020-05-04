689e3b966d9384ce654afee6792bdd1665ce9176
rashank <ravi.shankar@publicissapient.com>
2020-05-04 17:07:25 +0530

    Changes

--- currying.js
+++ currying.js
@@ -1,6 +1,9 @@
-
-const ffun = function(a, b, c) {
-    return a + b + c;
+const pipe = function(...fns) {
+    return function(x) {
+        return fns.reduce(function(v, f) {
+            return f(v);
+        }, x);
+    }
 };
 
 function curry(fn,arity = fn.length) {
@@ -14,37 +17,19 @@ function curry(fn,arity = fn.length) {
                 return nextCurried( args );
             }
         };
-    })( [5] );
+    })( [] );
 }
 
-const pipe = function(...fns) {
-    return function(x) {
-        return fns.reduce(function(v, f) {
-            return f(v);
-        }, x);
-    }
-};
+const addNums = (a, b, c) => a + b + c;
 
-const gfun = function(d, e) {
-    return d + e;
-};
+const multiplyNums = (a, b, c) => a * b * c;
 
-const hfun = function(f, g, h) {
-    return f + g + h;
-};
+const squareNum = x => x * x;
 
-/*const curriedF = curry(ffun);
-const curriedG = curry(gfun);
-const curriedH = curry(hfun);
+const numTransformPipeline = pipe(
+    squareNum,
+    curry(addNums)(2)(3),
+    curry(multiplyNums)(2)(3)
+);
 
-const newFun = pipe (
-    curriedF(1)(2),
-    curriedG(4),
-    curriedH(5)(6));*/
-
-const newFun = pipe (
-    curry(ffun)(1)(2),
-    curry(gfun)(4),
-    curry(hfun)(5)(6));
-
-console.log(newFun(10));    
+console.log(numTransformPipeline(2));
--- functionComposition.js
+++ functionComposition.js
@@ -1,16 +1,8 @@
-var add2 = x => x +2;
+var add3 = x => x + 3;
 
 var subtract2 = x => x - 2;
 
 var squareself = x => x * x;
-
-const pipe2 = function(...fns) {
-    return function(x) {
-        return fns.reduce(function(v, f) {
-            return f(v);
-        }, x);
-    }
-};
 
 const compose = function(...fns) {
     return function(x) {
@@ -27,12 +19,12 @@ const pipe = function (...fns){
         },x);
     }
 }
-console.log(squareself(subtract2(add2(3))));
+
 
 // Composing through pipe
-var prepareNum = pipe(add2, subtract2, squareself);
-var prepareNumCompose = pipe(squareself, subtract2, add2);
+console.log(squareself(subtract2(add3(3))));
+var prepareNum = pipe(add3, subtract2, squareself);
+var prepareNumCompose = compose(squareself, subtract2, add3);
 
 console.log(prepareNum(3));
 console.log(prepareNumCompose(3));
-
